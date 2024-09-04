7db5a9131160556973abd8b0c6acbd1d03b3813c
Gurgen <gbg.goris@gmail.com>
2024-09-04 23:09:13 +0400

    final commit

--- src/class1.java
+++ src/class1.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter a four digit number");
         int a = sc.nextInt();
-        System.out.println(a);
         System.out.println(a / 1000);
         System.out.println((a % 1000)/100);
         System.out.println(((a % 1000)/10) % 10);
