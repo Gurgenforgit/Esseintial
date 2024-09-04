ee424d5b2ebdb06695f3e624bc80d8b372532e4c
Gurgen <gbg.goris@gmail.com>
2024-09-04 22:27:30 +0400

    rewrited

--- src/class1.java
+++ src/class1.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class class1 {
     public static void main(String[] args) {
-        int a = 9876;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a);
         System.out.println(a / 1000);
         System.out.println((a % 1000)/100);
         System.out.println(((a % 1000)/10) % 10);
