feefcef19d70fc93c564cc264dded69a82145423
Gurgen <gbg.goris@gmail.com>
2024-09-04 16:00:00 +0400

    the unnecessary condition removed

--- src/class3.java
+++ src/class3.java
@@ -1,8 +1,7 @@
 public class class3 { public static void main(String[] args) {
-    int a = 88;
-    int b =  -11;
-    if(a % b == 0) System.out.println("true");
+    int a = 0;
+    int b =  11;
+    if(a % b == 0) System.out.println(7>5);// յանի ուզում եմ ասեմ, որ սրա ձևն էլ գիտեմ))
     if(a % b > 0)  System.out.println("false");
-    if(a % b < 0) System.out.println("false");
-}
+    }
 }
