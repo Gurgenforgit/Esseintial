07ff20f685c88e4dd1a403a5538be01f5a70174d
Gurgen <gbg.goris@gmail.com>
2024-09-04 11:20:03 +0400

    commit for 3

---
+++ src/class3.java
@@ -0,0 +1,8 @@
+public class class3 { public static void main(String[] args) {
+    int a = 88;
+    int b =  -11;
+    if(a % b == 0) System.out.println("true");
+    if(a % b > 0)  System.out.println("false");
+    if(a % b < 0) System.out.println("false");
+}
+}
