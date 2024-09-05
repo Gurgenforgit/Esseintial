1869680f60df2ff6a6be64d68e2a78a0363a3d57
Gurgen <gbg.goris@gmail.com>
2024-09-05 11:18:10 +0400

    scanner closed

--- src/class1.java
+++ src/class1.java
@@ -4,6 +4,7 @@ public class class1 {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter a four digit number");
         int a = sc.nextInt();
+        sc.close();
         System.out.println(a / 1000);
         System.out.println((a % 1000)/100);
         System.out.println(((a % 1000)/10) % 10);
