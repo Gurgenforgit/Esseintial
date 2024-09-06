f12c6c4f8f70a57caf715e323e17ae2523d76f1c
Gurgen <gbg.goris@gmail.com>
2024-09-07 00:35:01 +0400

    The second part of ex2 is done

---
+++ SeperateFractionalPartOfSumOfThreeDoubles.java
@@ -0,0 +1,10 @@
+public class SeperateFractionalPartOfSumOfThreeDoubles {
+    public static void main(String[] args) {
+    double a = 5.4;
+    double b = 9.8;
+    double c = 11.3;
+    double f = (a+b+c);
+    double e = (int)f;
+    System.out.println(f-e);
+}
+}
