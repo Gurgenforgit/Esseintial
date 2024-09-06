6ef9cb09018f7703bd21a8e34e5c855a4436b13e
Gurgen <gbg.goris@gmail.com>
2024-09-07 00:38:25 +0400

    The3-rd part of ex.2 is done

---
+++ SeperateWholePartOfSumOfWholePartsOfThreeDoubles.java
@@ -0,0 +1,8 @@
+public class SeperateWholePartOfSumOfWholePartsOfThreeDoubles {
+    public static void main(String[] args) {
+        double a = 5.4;
+        double b = 9.8;
+        double c = 11.3;
+        System.out.println((int)a + (int)b + (int)c );
+    }
+}
