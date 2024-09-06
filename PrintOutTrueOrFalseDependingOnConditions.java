5b9191960031d2f84edb70421e02825f9d8c0956
Gurgen <gbg.goris@gmail.com>
2024-09-07 00:45:28 +0400

    The ex3 is done

---
+++ PrintOutTrueOrFalseDependingOnConditions.java
@@ -0,0 +1,8 @@
+public class PrintOutTrueOrFalseDependingOnConditions {
+    public static void main(String[] args) {
+        int a = 99;
+        int b =  11;
+        if(a % b == 0) System.out.println(7>5);// յանի ուզում եմ ասեմ, որ սրա ձևն էլ գիտեմ))
+        if(a % b > 0)  System.out.println("false");
+    }
+}
