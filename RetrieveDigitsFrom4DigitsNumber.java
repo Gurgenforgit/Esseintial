5188efb02faca2a2fcf41d8e7f38bbf7b806aecf
Gurgen <gbg.goris@gmail.com>
2024-09-07 17:06:03 +0400

    condition added and the programm can not receive not four digits number

--- RetrieveDigitsFrom4DigitsNumber.java
+++ RetrieveDigitsFrom4DigitsNumber.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 public class RetrieveDigitsFrom4DigitsNumber {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("enter a four digit number");
-    int a = sc.nextInt();
-    sc.close();
-    System.out.println(a / 1000);
-    System.out.println((a % 1000)/100);
-    System.out.println(((a % 1000)/10) % 10);
-    System.out.println((a % 100) % 10);
-  }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a four digit number");
+        int FourDigitNumber = sc.nextInt();
+        if (FourDigitNumber > 9999 || FourDigitNumber < 1000) {
+            System.exit(1);
+        } else {
+            System.out.println(FourDigitNumber / 1000);
+            System.out.println((FourDigitNumber % 1000) / 100);
+            System.out.println(((FourDigitNumber % 1000) / 10) % 10);
+            System.out.println((FourDigitNumber % 100) % 10);
+            sc.close();
+        }
+    }
 }
