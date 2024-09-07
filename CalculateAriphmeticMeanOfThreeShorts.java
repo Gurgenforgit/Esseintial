a84abafa6410c738e3f27b505fc98008498edb2d
Gurgen <gbg.goris@gmail.com>
2024-09-08 03:30:30 +0400

    unnecessary elses removed, some introductiont textes added to printed result, and one condition added which doesn't allow division by zero

--- CalculateAriphmeticMeanOfThreeShorts.java
+++ CalculateAriphmeticMeanOfThreeShorts.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class CalculateAriphmeticMeanOfThreeShorts {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,31 +8,15 @@ public class CalculateAriphmeticMeanOfThreeShorts {
         short Number2 = sc.nextShort();
         short Number3 = sc.nextShort();
         int Mean = (Number1 + Number2 + Number3) / 3;
-        System.out.println(Mean);
-        if(Number1 > 0) {
-            Number1 = Number1;
-        }
-        else {
-            Number1 = (short) (-1 * Number1);
-        }
-        if(Number2 > 0) {
-            Number2 = Number2;
-        }
-        else {
-            Number2 = (short) (-1 * Number2);
-        }
-        if(Number3 > 0) {
-            Number3 = Number3;
-        }
-        else {
-            Number3 = (short) (-1 * Number3);
-        }
-        System.out.println((Number1 + Number2 + Number3) / Mean);
-
-
-
+        System.out.println("The arithmetical mean of input numbers is " + Mean);
+            if (Number1 < 0) Number1 = (short) (-1 * Number1);
+            if (Number2 < 0) Number2 = (short) (-1 * Number2);
+            if (Number3 < 0) Number3 = (short) (-1 * Number3);
+            if (Mean == 0){
+            System.out.println("Not possible to continue The operation because of division on zero is banned in Math.  ");
+        System.exit(0);}
+        System.out.println("The result of second required arithmetical operation is " +  (Number1 + Number2 + Number3) / Mean);
     }
-    }
-
+}
 
 
