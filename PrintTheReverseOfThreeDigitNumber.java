ff763eb397d12588efbd47adc8e34be1cd304ce5
Gurgen <gbg.goris@gmail.com>
2024-09-08 04:31:28 +0400

    all changes made according reviewer's comments

--- PrintTheReverseOfThreeDigitNumber.java
+++ PrintTheReverseOfThreeDigitNumber.java
@@ -1,23 +1,23 @@
 import java.util.Scanner;
 public class PrintTheReverseOfThreeDigitNumber {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input 3 digits number");
-        int ThreeDigitsNumber = sc.nextInt();
-        if (ThreeDigitsNumber > 999 || ThreeDigitsNumber < 100) {
-            System.exit(1);
-        } else {
-            int ThirdDigit = ((ThreeDigitsNumber % 100) % 10);
-            int SecondDigit = ((ThreeDigitsNumber % 100) / 10);
-            int FirstDigit = (ThreeDigitsNumber / 100);
-            int ReversedNumber = (ThirdDigit * 100 + SecondDigit * 10 + FirstDigit);
-            if (ThirdDigit == 0) {
-                System.out.println("It is not possible to reverse this number");
-            } else {
-                System.out.println("The reversed input number is " + ReversedNumber);
-            }
-        }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input 3 digits number");
+    int ThreeDigitsNumber = sc.nextInt();
+    if (ThreeDigitsNumber > 999 || ThreeDigitsNumber < 100) {
+      System.out.println("You didn't input three-digit number, Please next time input valid three-digit numbe ");
+      System.exit(1);}
+      int ThirdDigit = ((ThreeDigitsNumber % 100) % 10);
+      if (ThirdDigit == 0) {
+        System.out.println("It is not possible to reverse this number");
+      System.exit(0);}
+      int SecondDigit = ((ThreeDigitsNumber % 100) / 10);
+      int FirstDigit = (ThreeDigitsNumber / 100);
+      int ReversedNumber = (ThirdDigit * 100 + SecondDigit * 10 + FirstDigit);
+      System.out.println("The reversed input number is " + ReversedNumber);
+      }
     }
-}
+
+
 
 
