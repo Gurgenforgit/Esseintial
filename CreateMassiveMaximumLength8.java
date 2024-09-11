5c2d30ccc18b69bb45992c9b0b2250079d7b39cd
Gurgen <gbg.goris@gmail.com>
2024-09-11 14:25:10 +0400

    the condition corrected and some texts added to printout to be abvious what the result

--- CreateMassiveMaximumLength8.java
+++ CreateMassiveMaximumLength8.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class CreateMassiveMaximumLength8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input the length of the scanner which has to be in <1-8> range ");
+        System.out.println("Input the length of the array which has to be in <3-8> range ");
         int n = sc.nextInt();
-        if (n < 1 || n > 8) {
+        if (n < 3 || n > 8) {
             System.out.println("Not valid length for this array");
             System.exit(1);
         }
@@ -15,5 +15,8 @@ public class CreateMassiveMaximumLength8 {
         int b = sc.nextInt();
         Massive[(n / 2 - 1)] = a;
         Massive[(n / 2 + 1)] = b;
+        System.out.println("The length of the array is " + Massive.length);//optional
+        System.out.println("The element-" + (n / 2 - 1) + " " + "is " + a);//optional
+        System.out.println("The element-" + (n / 2 + 1) + " " + "is " + b);//optional
     }
 }
