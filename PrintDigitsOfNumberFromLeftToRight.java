b83980542096ffbd0e163d0e45114d90c55e3c54
Gurgen <gbg.goris@gmail.com>
2024-09-15 20:52:49 +0400

    'java-.iml' file is removed from '.gitignore'

--- PrintDigitsOfNumberFromLeftToRight.java
+++ PrintDigitsOfNumberFromLeftToRight.java
@@ -3,29 +3,27 @@ import java.util.Scanner;
 public class PrintDigitsOfNumberFromLeftToRight {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println( "Input any positive whole number");
+        System.out.println("Input any positive whole number");
         int a = sc.nextInt();
 
         // Preventing input not positive number
-        for(;a < 0;){
-       // if (a <= 0){
+        for (; a <= 0; ) {
             System.out.println(" Next time please input a Positive whole number");
-            System.out.println( "Input any positive whole number");
-             a = sc.nextInt();
+            System.out.println("Input any positive whole number");
+            a = sc.nextInt();
         }
-
         int b = a; // to keep input number for further operations
         int i = 1;
-        for (; a > 9 ; a /= 10) {
+        for (; a > 9; a /= 10) {
             i++;
         }
         // creating array of digits consisting of input number - from left to right
         int[] arr = new int[i];
-        for(; i > 0 ; i--)  {
-            arr[i-1] = b % 10;
-            b = b/10;
+        for (; i > 0; i--) {
+            arr[i - 1] = b % 10;
+            b = b / 10;
         }
-        for(int k = 0; k < arr.length ; k++){
+        for (int k = 0; k < arr.length; k++) {
             System.out.println(arr[k]);
         }
     }
