f6710b6c8b09aa2f24cdc512864a4981260b1c93
Gurgen <gbg.goris@gmail.com>
2024-09-18 13:40:35 +0400

    changed itteration place inside if condition

--- VariousArrayWithNegativeAndPositiveNumbers.java
+++ VariousArrayWithNegativeAndPositiveNumbers.java
@@ -1,16 +1,16 @@
 public class VariousArrayWithNegativeAndPositiveNumbers {
     public static void main(String[] args) {
         double[] arr = {5, -45, 2, 8, 0, 10};
-        double b = arr[0]; // by my logic 'b' should be 0, but if it is 0, the code doesn't run like it should, it skips add the first element
+        double b = 0; // by my logic 'b' should be 0, but if it is 0, the code doesn't run like it should, it skips add the first element
         int i = 0;
         for (; i < arr.length; ) {
             if (arr[i] > 0) {
-                i++;
                 b = b + arr[i];
+                i++;
             }
             if (arr[i] < 0) {
-                i = i + 2;
                 b = b + arr[i];
+                i = i + 2;
             }
             if (arr[i] == 0) {
                 break;
