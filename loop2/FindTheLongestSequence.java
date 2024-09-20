f22683d4e57bb0c8922859e4bdc5bebb9799f2d8
Gurgen <gbg.goris@gmail.com>
2024-09-20 22:51:03 +0400

    Now tho longest positive sequence could be 0

--- loop2/FindTheLongestSequence.java
+++ loop2/FindTheLongestSequence.java
@@ -3,8 +3,8 @@ package loop2;
 public class FindTheLongestSequence {
     public static void main(String[] args) {
         int[] arr = {1, 2, 5, 8, 9, 2, 1, 2, 3, 4, 5, 6, 7, 78, 0, 2, 98};
-        int temp = 1;
-        int longest = 1;
+        int temp = 0;
+        int longest = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i + 1] > arr[i]) {
                 temp = temp + 1;
@@ -13,7 +13,7 @@ public class FindTheLongestSequence {
                 }
             }
             if (arr[i + 1] < arr[i]) {
-                temp = 1;
+                temp = 0;
                 continue;
             }
         }
