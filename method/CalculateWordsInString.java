38ecced56fb54538c717a8296ca503e0b5bfe471
Gurgen <gbg.goris@gmail.com>
2024-09-22 16:02:37 +0400

    This code calculate quantity of the words in the string

---
+++ method/CalculateWordsInString.java
@@ -0,0 +1,36 @@
+package method;
+
+// Ունեք սովորական String փոփոխական, որի մեջ անգլերեն տառերով տեքստա գրած պրոբելներով։։ Դուք պիտի գրեք մեթոդ,
+// որը կստանաս տվյալ String-ը որպես պարամետր ու կվերադարձնի String-ում բառերի քանակը։
+public class CalculateWordsInString {
+    public static void main(String[] args) {
+        String Example = "  Բարև ախպեր ջան, ներողություն     տունը   մեկը չիգա։    ";
+        int result = calculateWords(Example);
+        System.out.println(result);
+    }
+
+    static int calculateWords(String str) {
+        int quantity = 0;
+
+        // This fragment of code checking if there is 'space' in the beginning of the string
+        int k = 0;
+        while (str.charAt(k) == 32) {
+            k++;
+            if (str.charAt(k) != 32)
+                break;
+        }
+
+        boolean Space = false;
+        for (int i = k; i < str.length(); i++) {
+            // This condition is checking whether there are or more than one 'space' in between
+            if (Space && str.charAt(i) == 32)
+                continue;
+
+            if (str.charAt(i) == 32) {
+                quantity += 1;
+                Space = true;
+            } else Space = false;
+        }
+        return quantity;
+    }
+}
