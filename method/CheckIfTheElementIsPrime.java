3f5642bd905c5f6af91e5d1e430a94edacc92f71
Gurgen <gbg.goris@gmail.com>
2024-09-22 16:39:14 +0400

    This code checks whether the choosed element of array is prime or not

--- method/CheckIfTheElementIsPrime.java
+++ method/CheckIfTheElementIsPrime.java
@@ -1,14 +1,16 @@
 package method;
+//Ձեզնից պահանջվումա գրել ծրագիր, որը կստուգի array-ի հերթական անդամի պարզ լինել-չլինելը
+// ու կտպի համապատասխան հաղորդագրություն։
 
 public class CheckIfTheElementIsPrime {
     public static void main(String[] args) {
         int[] arr = {1, 5, 12, 29};
-        boolean yourChoice = primeOrNote(arr[2]);
+        boolean yourChoice = primeOrNote(arr[3]);
         System.out.println("If you are searching a prime number it is " + yourChoice);
     }
     public static boolean primeOrNote (int x){
         boolean IsPrime = true;
-        for(int i = 2; i < x/2; i++){
+        for(int i = 2; i <= x/2; i++){
             if(x % i == 0)
                 IsPrime = false;
         }
